815a5d504b018ff74057d7ca70aece13b53a299c
MobeenShahzad <mobeenshazad32@gmail.com>
2021-10-27 13:39:05 +0500

    added assets

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 
 void main(){
@@ -31,7 +32,7 @@ return Scaffold(
     child: Column(
 
     children: [
-     SizedBox(height: 50),
+     SizedBox(height: 10),
       ClipRRect(
         borderRadius: BorderRadius.circular(50),
 
@@ -43,16 +44,17 @@ return Scaffold(
             shape: BoxShape.circle,
           ),
           child: Image(
-            fit: BoxFit.cover,
-            image: NetworkImage(
+            image: AssetImage('assets/profile-1.png'
 
-              'https://i.pinimg.com/550x/7d/1a/3f/7d1a3f77eee9f34782c6f88e97a6c888.jpg'
+
             ),
+
+           fit: BoxFit.cover,
             ),
           ),
 
         ),
-      SizedBox(height: 10),
+      SizedBox(height: 5),
       Center(
         child: Text
           ("Mobeen",
@@ -69,7 +71,7 @@ return Scaffold(
           color: Colors.black87,
         ),
       ),
-      SizedBox(height: 10),
+      SizedBox(height: 5),
       Row (
         mainAxisAlignment: MainAxisAlignment.center,
         children:
@@ -127,7 +129,208 @@ return Scaffold(
 
 
         ],
-      )
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 25.0),
+        child: Container(
+          height: 50,
+          width: 250,
+
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+              color: Color(0xFF2B2828),
+          ),
+          child: Center(
+            child: Text(
+              "Follow",style: TextStyle(
+              fontSize: 17,
+              fontWeight: FontWeight.w600,
+              color: Color(0xFFF73628),
+
+            ),
+
+            ),
+          ),
+        ),
+      ),
+
+
+      Expanded(
+        child: Container(
+
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+
+
+            ),
+
+              gradient: LinearGradient(
+
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Colors.grey, Colors.black87])),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+
+                    child: Container(
+
+                      height: 130,
+                      width: 130,
+
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: Image(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+
+                            'https://i.pinimg.com/550x/7d/1a/3f/7d1a3f77eee9f34782c6f88e97a6c888.jpg'
+                        ),
+                      ),
+                    ),
+
+                  ),
+
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+
+                    child: Container(
+                      height: 130,
+                      width: 130,
+
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: Image(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+
+                            'https://i.pinimg.com/550x/7d/1a/3f/7d1a3f77eee9f34782c6f88e97a6c888.jpg'
+                        ),
+                      ),
+                    ),
+
+                  ),
+
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+
+                    child: Container(
+
+                      height: 130,
+                      width: 130,
+
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: Image(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+
+                            'https://i.pinimg.com/550x/7d/1a/3f/7d1a3f77eee9f34782c6f88e97a6c888.jpg'
+                        ),
+                      ),
+                    ),
+
+                  ),
+
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+
+                    child: Container(
+                      height: 130,
+                      width: 130,
+
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: Image(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+
+                            'https://i.pinimg.com/550x/7d/1a/3f/7d1a3f77eee9f34782c6f88e97a6c888.jpg'
+                        ),
+                      ),
+                    ),
+
+                  ),
+
+                ],
+              ),
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+
+                    child: Container(
+
+                      height: 130,
+                      width: 130,
+
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: Image(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+
+                            'https://i.pinimg.com/550x/7d/1a/3f/7d1a3f77eee9f34782c6f88e97a6c888.jpg'
+                        ),
+                      ),
+                    ),
+
+                  ),
+
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+
+                    child: Container(
+                      height: 130,
+                      width: 130,
+
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: Image(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+
+                            'https://i.pinimg.com/550x/7d/1a/3f/7d1a3f77eee9f34782c6f88e97a6c888.jpg'
+                        ),
+                      ),
+                    ),
+
+                  ),
+
+                ],
+              )
+
+            ],
+
+          )
+
+        ),
+
+      ),
     ],
   ),
 
